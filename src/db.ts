5bce54b0cf7aaf0800c16728374e73c13f501fcb
Rhodanthe1116 <z0916945857@gmail.com>
2022-06-05 21:25:40 +0800

    deploy

--- src/db.ts
+++ src/db.ts
@@ -3,12 +3,13 @@ import mongoose from 'mongoose'
 const uri = process.env.ATLAS_URI
 if (!uri) {
   console.log('no atlas uri')
+} else {
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
 }
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-})
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
